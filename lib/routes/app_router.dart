55d96009558e5a07c41df54ef50da2ee6b58c289
coarat <9015one@gmail.com>
2025-08-05 18:45:38 +0900

    routerを整理

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -1,16 +1,14 @@
 import 'package:go_router/go_router.dart';
-import '../features/home/ui/home.dart';
-import '../features/user/ui/create_user.dart';
+import 'package:life_track/features/home/ui/home.dart';
+import 'package:life_track/features/user/ui/create_user.dart';
 
-final router = GoRouter(
-  routes: [
-    GoRoute(
-      path: '/',
-      builder: (context, state) => const HomeScreen(),
-    ),
-    GoRoute(
-      path: '/user/create',
-      builder: (context, state) => const CreateUserScreen(),
-    ),
-  ],
-);
+final appRoutes = [
+  GoRoute(
+    path: '/',
+    builder: (context, state) => const HomeScreen(),
+  ),
+  GoRoute(
+    path: '/user/create',
+    builder: (context, state) => const CreateUserScreen(),
+  ),
+];
